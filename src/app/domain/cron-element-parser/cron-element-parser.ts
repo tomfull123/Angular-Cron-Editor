abf3807978f3aa3a41dfca32c834e3092d92cc1f
tomfull123 <tom_full@hotmail.co.uk>
2021-09-04 23:56:32 +0100

    Implemented a lot of the cron editor component.

--- src/app/domain/cron-element-parser/cron-element-parser.ts
+++ src/app/domain/cron-element-parser/cron-element-parser.ts
@@ -1,24 +1,46 @@
 import {CronElementToken} from "../cron-element-token/cron-element-token";
 import {CronElementLexer} from "../cron-element-lexer/cron-element-lexer";
+import {Injectable} from "@angular/core";
 
+export enum CronElementIndex {
+  Minute = 0,
+  Hour = 1,
+  DayOfWeek = 4,
+  DayOfMonth = 2,
+  Month = 3
+}
+
+@Injectable({
+  providedIn: 'root'
+})
 export class CronElementParser {
 
-  parseCronElement(cron: string, elementIndex: number) {
+  isCronElementValid(elementIndex: CronElementIndex, cron?: string): boolean {
+    if(cron === null) return false;
+    const cronElements = cron!.split(' ');
+    if(elementIndex >= cronElements.length) return false;
+    const cronElement = cronElements[elementIndex];
+    if(cronElement === '') return false;
+    const parsedCronElement = this.parseCronElement(cron!, elementIndex)?.join('');
+    return cronElement === parsedCronElement;
+  }
+
+  parseCronElement(cron: string, elementIndex: CronElementIndex): string[] | undefined {
     const cronElements = cron.split(' ');
 
-    if(elementIndex >= cronElements.length) return
+    if (elementIndex >= cronElements.length) return;
     const cronElement = cronElements[elementIndex];
 
     const tokens = this.parseTokens(cronElement)
 
-    return tokens;
+    return tokens.map(t => t.value);
   }
 
   private parseTokens(cronElement: string): CronElementToken[] {
     const lexer = new CronElementLexer(cronElement);
     const tokens = [];
 
-    while(!lexer.isEnd()) {
+    while (!lexer.isEnd()) {
       tokens.push(lexer.readNext());
     }
 
--- src/app/presentation/cron-editor/cron-editor.component.ts
+++ src/app/presentation/cron-editor/cron-editor.component.ts
@@ -1,5 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {CronElementMode} from "../cron-element-mode-selector/cron-element-mode-selector.component";
+import {CronElementIndex, CronElementParser} from "../../domain/cron-element-parser/cron-element-parser";
+import {InputType} from "../multi-select/multi-select.component";
 
 @Component({
   selector: 'app-cron-editor',
@@ -9,10 +11,15 @@ import {CronElementMode} from "../cron-element-mode-selector/cron-element-mode-s
 export class CronEditorComponent implements OnInit {
 
   private _cron: string = '';
+  CronElementMode = CronElementMode;
+  CronElementIndex = CronElementIndex;
+  InputType = InputType;
+  currentTabIndex = 0;
+  _cronInputCursorPosition = -1
 
   @Input()
   set cron(cron: string) {
-    if(this._cron !== cron) {
+    if (this._cron !== cron) {
       this._cron = cron;
       this.cronChange.emit(cron);
     }
@@ -25,17 +32,148 @@ export class CronEditorComponent implements OnInit {
   @Output()
   cronChange = new EventEmitter<string>();
 
+  @Output()
+  selectedCronElementIndexChange = new EventEmitter<number>();
+
+  get minuteValues(): string[] {
+    return this.getCronElementValues(CronElementIndex.Minute);
+  }
+
+  set minuteValues(minutes) {
+    this.setCronElementValues(CronElementIndex.Minute, minutes, this.minuteMode);
+  }
+
+  get hourValues(): string[] {
+    return this.getCronElementValues(CronElementIndex.Hour);
+  }
+
+  set hourValues(hours) {
+    this.setCronElementValues(CronElementIndex.Hour, hours, this.hourMode);
+  }
+
+  get dayOfWeekValues(): string[] {
+    return this.getCronElementValues(CronElementIndex.DayOfWeek);
+  }
+
+  set dayOfWeekValues(daysOfWeek) {
+    this.setCronElementValues(CronElementIndex.DayOfWeek, daysOfWeek, this.dayOfWeekMode);
+  }
+
+  get dayOfMonthValues(): string[] {
+    return this.getCronElementValues(CronElementIndex.DayOfMonth);
+  }
+
+  set dayOfMonthValues(daysOfMonth) {
+    this.setCronElementValues(CronElementIndex.DayOfMonth, daysOfMonth, this.dayOfMonthMode);
+  }
+
+  get monthValues(): string[] {
+    return this.getCronElementValues(CronElementIndex.Month);
+  }
+
+  set monthValues(months) {
+    this.setCronElementValues(CronElementIndex.Month, months, this.monthMode);
+  }
+
+  get selectedCronElementIndex(): number {
+    switch (this.currentTabIndex) {
+      case 0: // Minutes
+        return CronElementIndex.Minute;
+      case 1: // Hours
+        return CronElementIndex.Hour;
+      case 2: // Day of week
+        return CronElementIndex.DayOfWeek;
+      case 3: // Day of month
+        return CronElementIndex.DayOfMonth;
+      case 4: // Month
+        return CronElementIndex.Month;
+      case 5: // Advanced
+        const cronElements = this.cron.split(' ');
+        let cronLengthTotal = 0;
+        for (let i = 0; i < cronElements.length; i++) {
+          const element = cronElements[i];
+          if (this.cronInputCursorPosition >= cronLengthTotal && this.cronInputCursorPosition <= cronLengthTotal + element.length) {
+            return i;
+          }
+          cronLengthTotal += element.length + 1;
+        }
+    }
+
+    return -1;
+  }
+
+  get cronInputCursorPosition() {
+    return this._cronInputCursorPosition;
+  }
+
+  set cronInputCursorPosition(cronInputCursorPosition) {
+    if(this._cronInputCursorPosition !== cronInputCursorPosition) {
+      this._cronInputCursorPosition = cronInputCursorPosition;
+      this.updateSelectedCronElementIndexChange();
+    }
+  }
+
   minuteMode = CronElementMode.Every;
   hourMode = CronElementMode.Every;
   dayOfWeekMode = CronElementMode.Every;
   dayOfMonthMode = CronElementMode.Every;
   monthMode = CronElementMode.Every;
 
-
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private cronElementParser: CronElementParser) {
   }
 
+  ngOnInit(): void {
+    this.updateSelectedCronElementIndexChange();
+  }
+
+  private getCronElementValues(cronIndex: number): string[] {
+    const cronElement = this.cronElementParser.parseCronElement(this.cron, cronIndex)?.join('');
+    if (cronElement == null || cronElement === '*') return [];
+    const valueList = cronElement.split(',').filter(v => v !== '');
+    return valueList.map(v => {
+      const stepValues = v.split('/');
+      if (stepValues.length === 2) return stepValues[1];
+      return v;
+    })
+  }
+
+  private setCronElementValues(elementIndex: CronElementIndex, values: string[], cronElementMode: CronElementMode) {
+    const cronElements = this.cron.split(' ');
+
+    if (values.length === 0) {
+      cronElements[elementIndex] = '*';
+    } else {
+      if (cronElementMode === CronElementMode.Every) {
+        cronElements[elementIndex] = values.map(v => `*/${v}`).join(',');
+      } else {
+        cronElements[elementIndex] = values.join(',');
+      }
+    }
+
+    this.cron = cronElements.join(' ');
+  }
+
+  refreshCronElement(elementIndex: CronElementIndex, mode: CronElementMode) {
+    this.setCronElementValues(elementIndex, this.getCronElementValues(elementIndex), mode);
+  }
+
+  clearCronElement(elementIndex: CronElementIndex) {
+    const cronElements = this.cron.split(' ');
+    cronElements[elementIndex] = '*';
+    this.cron = cronElements.join(' ');
+  }
+
+  resetCursorPos() {
+    this.cronInputCursorPosition = -1
+  }
+
+  getCursorPos(cronInput: HTMLInputElement) {
+    if (cronInput.selectionStart != null) {
+      this.cronInputCursorPosition = cronInput.selectionStart;
+    }
+  }
+
+  updateSelectedCronElementIndexChange() {
+    this.selectedCronElementIndexChange.emit(this.selectedCronElementIndex);
+  }
 }
