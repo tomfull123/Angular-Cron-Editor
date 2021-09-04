775ac49b39903c7b8310b9373f1ac883b2550989
tomfull123 <tom_full@hotmail.co.uk>
2021-09-04 13:15:59 +0100

    Started implementing the cron editor component.

---
+++ src/app/presentation/cron-editor/cron-editor.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CronEditorComponent } from './cron-editor.component';
+
+describe('CronEditorComponent', () => {
+  let component: CronEditorComponent;
+  let fixture: ComponentFixture<CronEditorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CronEditorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CronEditorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/presentation/cron-editor/cron-editor.component.ts
@@ -0,0 +1,41 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {CronElementMode} from "../cron-element-mode-selectorx/cron-element-mode-button-selector.component";
+
+@Component({
+  selector: 'app-cron-editor',
+  templateUrl: './cron-editor.component.html',
+  styleUrls: ['./cron-editor.component.css']
+})
+export class CronEditorComponent implements OnInit {
+
+  private _cron: string = '';
+
+  @Input()
+  set cron(cron: string) {
+    if(this._cron !== cron) {
+      this._cron = cron;
+      this.cronChange.emit(cron);
+    }
+  }
+
+  get cron() {
+    return this._cron;
+  }
+
+  @Output()
+  cronChange = new EventEmitter<string>();
+
+  minuteMode = CronElementMode.Every;
+  hourMode = CronElementMode.Every;
+  dayOfWeekMode = CronElementMode.Every;
+  dayOfMonthMode = CronElementMode.Every;
+  monthMode = CronElementMode.Every;
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
