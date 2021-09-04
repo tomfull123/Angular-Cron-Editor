0583acfd80b28f6603acb2528b28c55c7b293d81
tomfull123 <tom_full@hotmail.co.uk>
2021-09-04 13:16:39 +0100

    Changed the app component to now display the cron editor.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CronEditorComponent } from './presentation/cron-editor/cron-editor.component';
+import { CronDescriptionComponent } from './presentation/cron-description/cron-description.component';
+import { CronElementModeButtonSelectorComponent } from './presentation/cron-element-mode-selectorx/cron-element-mode-button-selector.component';
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatTabsModule} from "@angular/material/tabs";
+import { CronElementModeSelectorComponent } from './presentation/cron-element-mode-selector/cron-element-mode-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CronEditorComponent,
+    CronDescriptionComponent,
+    CronElementModeButtonSelectorComponent,
+    CronElementModeSelectorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonToggleModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
